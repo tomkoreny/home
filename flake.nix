66556e9f23c4dbde7e17ecaabe3b06f69e850a9a
Tom Koreny <tom@tomkoreny.com>
2024-12-20 12:37:20 +0100

    first commit

---
+++ flake.nix
@@ -0,0 +1,72 @@
+{
+    inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+        snowfall-lib = {
+            url = "github:snowfallorg/lib";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    hyprland.url = "github:hyprwm/Hyprland";
+    stylix.url = "github:danth/stylix";
+    nixvim.url = "github:nix-community/nixvim";
+    nixvim.inputs.nixpkgs.follows = "nixpkgs";
+    lanzaboote = {
+      url = "github:nix-community/lanzaboote/v0.4.1";
+
+      # Optional but recommended to limit the size of your system closure.
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    };
+
+    outputs = inputs:
+        inputs.snowfall-lib.mkFlake {
+            inherit inputs;
+            src = ./.;
+	    
+# Add modules to all NixOS systems.
+	    systems.modules.nixos = with inputs; [
+      hyprland.nixosModules.default
+      stylix.nixosModules.stylix
+      nixvim.nixosModules.nixvim
+      lanzaboote.nixosModules.lanzaboote
+      home-manager.nixosModules.default
+	    ];
+
+# If you wanted to configure a Darwin (macOS) system.
+# systems.modules.darwin = with inputs; [
+#   my-input.darwinModules.my-module
+# ];
+
+# Add a module to a specific host.
+	    systems.hosts.nixos.modules = with inputs; [
+# my-input.nixosModules.my-module
+	    ];
+
+# Add a custom value to `specialArgs`.
+	    systems.hosts.nixos.specialArgs = {
+		    my-custom-value = "my-value";
+	    };
+
+            # Configure Snowfall Lib, all of these settings are optional.
+            snowfall = {
+                # Tell Snowfall Lib to look in the `./nix/` directory for your
+                # Nix files.
+                root = ./.;
+
+                # Choose a namespace to use for your flake's packages, library,
+                # and overlays.
+                namespace = "tomkoreny";
+
+                # Add flake metadata that can be processed by tools like Snowfall Frost.
+                meta = {
+                    # A slug to use in documentation when displaying things like file paths.
+                    name = "tomk-dots";
+
+                    # A title to show for your flake, typically the name.
+                    title = "Tom Koreny's NixOS config";
+                };
+            };
+        };
+}
---
+++ systems/x86_64-linux/nixos/default.nix
@@ -0,0 +1,352 @@
+{
+    # Snowfall Lib provides a customized `lib` instance with access to your flake's library
+    # as well as the libraries available from your flake's inputs.
+    lib,
+    # An instance of `pkgs` with your overlays and packages applied is also available.
+    pkgs,
+    # You also have access to your flake's inputs.
+    inputs,
+
+    # Additional metadata is provided by Snowfall Lib.
+    namespace, # The namespace used for your flake, defaulting to "internal" if not set.
+    system, # The system architecture for this host (eg. `x86_64-linux`).
+    target, # The Snowfall Lib target for this system (eg. `x86_64-iso`).
+    format, # A normalized name for the system target (eg. `iso`).
+    virtual, # A boolean to determine whether this system is a virtual target using nixos-generators.
+    systems, # An attribute map of your defined hosts.
+
+    # All other arguments come from the system system.
+    config,
+    ...
+}:
+{
+    # Your configuration.
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  swapDevices = [{
+    device = "/swapfile";
+    size = 16 * 1024; # 16GB
+  }];
+
+# this section is wierd and does not work, fix one day
+  i18n.inputMethod = {
+	  enable = true;
+	  type = "ibus";
+	  ibus.engines = with pkgs.ibus-engines; [ /* any engine you want, for example */ uniemoji ];
+  };
+
+# end of section
+
+  # Bootloader.
+  boot.plymouth.enable = true;
+  boot.loader.systemd-boot.enable = lib.mkForce false;
+  boot.loader.systemd-boot.configurationLimit = 5;
+  #boot.loader.systemd-boot.netbootxyz.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+
+            boot.lanzaboote = {
+              enable = true;
+              pkiBundle = "/etc/secureboot";
+            };
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.gc = { automatic = true; dates = "weekly"; options = "--delete-older-than 7d"; };
+
+
+  networking.hostName = "nixos"; # Define your hostname.
+  networking.extraHosts = (builtins.readFile ./config/hosts/hosts);
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Prague";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "cs_CZ.UTF-8";
+    LC_IDENTIFICATION = "cs_CZ.UTF-8";
+    LC_MEASUREMENT = "cs_CZ.UTF-8";
+    LC_MONETARY = "cs_CZ.UTF-8";
+    LC_NAME = "cs_CZ.UTF-8";
+    LC_NUMERIC = "cs_CZ.UTF-8";
+    LC_PAPER = "cs_CZ.UTF-8";
+    LC_TELEPHONE = "cs_CZ.UTF-8";
+    LC_TIME = "cs_CZ.UTF-8";
+  };
+
+  services.tailscale.enable = true;
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the XFCE Desktop Environment.
+  services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
+  xdg = {
+	  portal = {
+		  enable = true;
+	  };
+  };
+
+  security.sudo.extraRules= [
+  {  users = [ "tom" ];
+	  commands = [
+	  { command = "ALL" ;
+		  options= [ "NOPASSWD" ]; # "SETENV" # Adding the following could be a good idea
+	  }
+	  ];
+  }
+  ];
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.tom = {
+    isNormalUser = true;
+    description = "Tom Koreny";
+    extraGroups = [ "networkmanager" "wheel" "docker" ];
+    packages = with pkgs; [
+    ];
+  };
+
+  # Maaaybe make this home manager somehow someday
+  # maybe make some proper config, inspire from lazyvim
+  programs.nixvim = {
+	  enable = true;
+
+	  plugins.lualine.enable = true;
+  };
+
+
+  home-manager.useUserPackages = true;
+  home-manager.useGlobalPkgs = true;
+  home-manager.users.tom = { pkgs, ... }: {
+	  home.packages = [
+		  pkgs.nerdfonts
+			  pkgs.nerdfetch
+			  pkgs.sshpass
+			  pkgs.ansible
+			  pkgs.atool
+			  pkgs.openfortivpn
+			  pkgs.kubectl
+			  pkgs.httpie
+			  pkgs.docker
+			  pkgs.wl-clipboard
+			  pkgs.nodejs
+			  pkgs.node-gyp
+			  pkgs.tiramisu
+			  pkgs.teams-for-linux
+			  pkgs.jetbrains.webstorm
+			  pkgs.beeper
+
+              (pkgs.discord.override {
+                # withOpenASAR = true; # can do this here too
+                withVencord = true;
+              })
+
+			  pkgs.hypridle
+			  pkgs.git-credential-oauth
+			  ];
+
+	  home.shellAliases = {
+		  v = "nvim";
+		  ssh = "kitten ssh";
+		  vi = "nvim";
+		  conf = "nvim ~/nixos/configuration.nix";
+		  sw = "nh os switch";
+		  dcu = "docker compose up -d";
+		  dcd = "docker compose down";
+		  dc = "docker compose";
+		  dev = "npm run dev";
+		  start = "npm run start";
+	  };
+	  programs.nh = {
+		  enable = true;
+		  clean.enable = true;
+		  clean.extraArgs = "--keep-since 4d --keep 3";
+		  flake = "/home/tom/nixos";
+	  };
+
+
+	  programs.bash.enable = true;
+	  programs.bun.enable = true;
+	  programs.git = {
+		  enable = true;
+		  userEmail = "tom@tomkoreny.com";
+		  userName = "Tom Koreny";
+		  extraConfig = {
+			  credential.helper = "oauth";
+			  push = { autoSetupRemote = true; };
+		  };
+	  };
+	  programs.starship.enable = true;
+	  programs.k9s.enable = true;
+
+	  programs.waybar = {
+		  enable = true;
+		  settings = lib.importJSON ./config/waybar/config.json;
+	  };
+
+	  home.file.".config/waybar/scripts/polytiramisu.sh" = {
+		  source = ./config/waybar/polytiramisu.sh;
+	  };
+# The state version is required and should stay at the version you
+# originally installed.
+	  home.stateVersion = "24.05";
+	  home.sessionVariables.NIXOS_OZONE_WL = "1";
+	  programs = {
+		  kitty.enable = true; # required for the default Hyprland config
+
+		  wofi.enable = true; # required for the default Hyprland config
+		  chromium = {
+			  enable = true;
+			  package = pkgs.google-chrome;
+			  #nvidia fix :(
+			  commandLineArgs = [
+		 		"--disable-gpu-compositing"
+			  ];
+		  };
+	  };
+
+  };
+
+# Enable automatic login for the user.
+  services.displayManager.autoLogin.enable = true;
+  services.displayManager.autoLogin.user = "tom";
+
+# Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+# List packages installed in system profile. To search, run:
+# $ nix search wget
+  environment.systemPackages = with pkgs; [
+    sbctl
+    mangohud
+    protonup
+  ];
+
+# Some programs need SUID wrappers, can be configured further or are
+# started in user sessions.
+# programs.mtr.enable = true;
+# programs.gnupg.agent = {
+#   enable = true;
+#   enableSSHSupport = true;
+# };
+
+# List services that you want to enable:
+
+# Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+# Open ports in the firewall.
+# networking.firewall.allowedTCPPorts = [ ... ];
+# networking.firewall.allowedUDPPorts = [ ... ];
+# Or disable the firewall altogether.
+# networking.firewall.enable = false;
+
+  services.hardware.openrgb = {
+	  enable = true;
+	  package = pkgs.openrgb-with-all-plugins;
+	  motherboard = "amd";
+	  server = {
+		  port = 6742;
+	  };
+  };
+
+# This value determines the NixOS release from which the default
+# settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
+
+  # Enable graphics driver in NixOS unstable/NixOS 24.11
+  hardware.graphics.enable = true;
+  hardware.graphics.enable32Bit = true;
+
+  # Load "nvidia" driver for Xorg and Wayland
+  services.xserver.videoDrivers = ["nvidia"];
+
+
+  hardware.nvidia = {
+
+    # Modesetting is required.
+    modesetting.enable = true;
+
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    # Enable this if you have graphical corruption issues or application crashes after waking
+    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
+    # of just the bare essentials.
+    powerManagement.enable = false;
+
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = false;
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of
+    # supported GPUs is at:
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+    # Only available from driver 515.43.04+
+    # Currently "beta quality", so false is currently the recommended setting.
+    open = true;
+
+    # Enable the Nvidia settings menu,
+    # accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
+  };
+
+  virtualisation.docker.rootless = {
+    enable = true;
+    setSocketVariable = true;
+  };
+
+
+  programs.steam.enable = true;
+  programs.steam.gamescopeSession.enable = true;
+  programs.gamemode.enable = true;
+
+ environment.sessionVariables = {
+    STEAM_EXTRA_COMPAT_TOOLS_PATHS =
+      "\${HOME}/.steam/root/compatibilitytools.d";
+  };
+
+}
