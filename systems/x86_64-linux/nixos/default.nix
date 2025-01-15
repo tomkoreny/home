04bb9143fc3d42162b520eb54d8d2d0fbd905826
Tom Koreny <tom@tomkoreny.com>
2025-01-15 14:44:42 +0100

    diff screen resolution

--- systems/x86_64-linux/nixos/default.nix
+++ systems/x86_64-linux/nixos/default.nix
@@ -249,4 +249,9 @@
   environment.sessionVariables = {
     STEAM_EXTRA_COMPAT_TOOLS_PATHS = "\${HOME}/.steam/root/compatibilitytools.d";
   };
+
+  nix.settings = {
+    substituters = ["https://hyprland.cachix.org"];
+    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+  };
 }
