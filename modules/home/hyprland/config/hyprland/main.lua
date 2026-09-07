d3d7456bee953d3224138b7767865e771f413e4c
Tom Koreny <tom@tomkoreny.com>
2026-09-07 15:56:49 +0200

    fix: disable mouse acceleration in Tom Linux session

--- modules/home/hyprland/config/hyprland/main.lua
+++ modules/home/hyprland/config/hyprland/main.lua
@@ -118,6 +118,7 @@ hl.config({
         kb_options = "",
         kb_rules = "",
         follow_mouse = 1,
+        accel_profile = "flat",
         sensitivity = 0,
         touchpad = { natural_scroll = false },
     },
